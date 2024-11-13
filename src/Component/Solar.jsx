a62810e295df81f22a68686ba30ccf954963832c
Adeline Vumiliya <iradukundaadeline550@gmail.com>
2024-11-13 16:02:04 +0200

    update link text

--- src/Component/Solar.jsx
+++ src/Component/Solar.jsx
@@ -4,7 +4,7 @@ function Solar() {
   return (
     <div className="bg-pink-600 grid grid-rows-2 ">
         <h1 className='text-center font-semibold text-gray-100'>Solar Panels</h1>
-      <a href='#' className='text-center text-white'>Schedule a Virtual Consultation</a>
+      <a href='#' className='text-center text-white'>Schedule a Virtual Consultating</a>
 
     </div>
   )
